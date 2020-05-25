d33aaa71fbd41fdc71fd5713a78c33bb90a79b9f
taemin <tmmoond8@gmail.com>
2020-05-26 01:01:38 +0900

    project init

---
+++ pages/index.tsx
@@ -0,0 +1,7 @@
+export default function IndexPage() {
+  return (
+    <div>
+      <p>This is index page</p>
+    </div>
+  )
+}
---
+++ pages/test.tsx
@@ -0,0 +1,7 @@
+export default function TestPage() {
+  return (
+    <div>
+      <p>This is test page</p>
+    </div>
+  )
+}
